0f832083b178e685eca4459bce796b3140255b90
rudyP123 <ruduspatel@gmail.com>
2024-01-25 18:42:26 -0500

    added other files

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+
+# FizzBuzz Program
+
+for number in range(1, 21):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
